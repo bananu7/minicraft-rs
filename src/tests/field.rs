a7dd80666bc05455ac658a6f65e268194e940cb2
bananu7 <bananu7@o2.pl>
2018-12-29 23:22:19 +0100

    Added a few more tests for coordinates

--- src/tests/field.rs
+++ src/tests/field.rs
@@ -1,12 +1,47 @@
 use crate::world::from_world_to_local;
 use crate::world::coord::{InnerChunkCoord, OuterChunkCoord};
+use crate::world::SIZE;
 
 use super::*;
 
+// Those helpers are just to shorten the code somewhat
+// Not that Rust could use unnamed uniform initialization
+// which I despised in C++ before but now, esp. for test
+// code actually seems nice. Well there are macros I guess...
+fn w(x: i64, y: i64, z: i64) -> WorldCoord {
+    WorldCoord::new(x, y, z)
+}
+
+fn o(x: i64, y: i64, z: i64) -> OuterChunkCoord {
+    OuterChunkCoord::new(x, y, z)
+}
+
+fn i(x: i64, y: i64, z: i64) -> InnerChunkCoord {
+    InnerChunkCoord::new(x, y, z)
+}
+
 #[test]
-fn test_from_world_to_local() {
+fn test_from_world_to_local_trivial() {
     assert_eq!(
-        from_world_to_local(WorldCoord::new(0,0,0)),
-        (OuterChunkCoord::new(0,0,0), InnerChunkCoord::new(0,0,0))
+        from_world_to_local(w(0,0,0)),
+        (o(0,0,0), i(0,0,0))
+    );
+
+    assert_eq!(
+        from_world_to_local(w(1,0,0)),
+        (o(0,0,0), i(1,0,0))
+    );
+}
+
+#[test]
+fn test_from_world_to_local_cross() {
+    assert_eq!(
+        from_world_to_local(w(SIZE,0,0)),
+        (o(1,0,0), i(0,0,0))
+    );
+
+    assert_eq!(
+        from_world_to_local(w(SIZE+1,0,0)),
+        (o(1,0,0), i(1,0,0))
     );
 }
