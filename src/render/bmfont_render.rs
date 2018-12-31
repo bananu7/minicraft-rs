da98600f78a11557021b7bfc0b1a1875ba98a016
bananu7 <bananu7@o2.pl>
2018-12-31 15:54:12 +0100

    Fixed character offset and spacing

--- src/render/bmfont_render.rs
+++ src/render/bmfont_render.rs
@@ -18,11 +18,12 @@ fn create_font_program(display : &glium::Display) -> glium::Program {
         330 => {
             vertex: "
                 #version 330 core
+                uniform vec2 offset;
                 in vec2 position;
                 in vec2 source;
                 out vec2 vSource;
                 void main() {
-                    gl_Position = vec4(position / 40.0, 0.0, 1.0);
+                    gl_Position = vec4((position + offset) / 320.0, 0.0, 1.0);
                     vSource = source;
                 }
             ",
@@ -67,24 +68,14 @@ impl DisplayFont {
             let xo = c.x_offset as f32;
             let yo = c.y_offset as f32;
 
-            /*
-            println!("x: {} xs: {}, w: {}", x, xs, w);
-            println!("y: {} ys: {}, h: {}", y, ys, h);
-
-            println!("y/ys: {} (y+h)/ys {}", y/ys, (y+h)/ys);*/
-
             char_to_num.insert(*i, num);
             num += 1;
 
             verts.append(&mut vec![
-                Vertex { position: [0.0, 0.0],
-                         source: [x / xs, 1.0 - (y+h)/ys] },
-                Vertex { position: [w, 0.0],
-                         source: [(x+w)/xs, 1.0 - (y+h)/ys] },
-                Vertex { position: [0.0, h],
-                         source: [x/xs, 1.0 - y / ys] },
-                Vertex { position: [w, h],
-                         source: [(x+w)/xs, 1.0 - y / ys] },
+                Vertex { position: [xo,     -yo      ], source: [x    /xs,  1.0 - y    /ys] },
+                Vertex { position: [xo + w, -yo      ], source: [(x+w)/xs,  1.0 - y    /ys] },
+                Vertex { position: [xo,     -(yo + h)], source: [x    /xs,  1.0 - (y+h)/ys] },
+                Vertex { position: [xo + w, -(yo + h)], source: [(x+w)/xs,  1.0 - (y+h)/ys] },
             ]);
         }
 
@@ -107,7 +98,9 @@ impl DisplayFont {
         }
     }
 
-    pub fn print(self: &Self, target: &mut glium::Frame, s: &str) -> Result<(), glium::DrawError> {
+    fn print_single(self: &Self, target: &mut glium::Frame, char_idx: usize, offset: [f32; 2])
+        -> Result<(), glium::DrawError>
+    {
         const NO_INDICES: glium::index::NoIndices =
             glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
 
@@ -119,17 +112,30 @@ impl DisplayFont {
                 [0.0, 0.0, 0.0, 1.0f32]
             ],
             tex: &self.texture,
+            offset: offset,
         };
 
         let draw_parameters = glium::DrawParameters {
             .. Default::default()
         };
 
+        target.draw(self.vbo.slice(char_idx..(char_idx+4)).unwrap(), &NO_INDICES, &self.program, &uniforms, &draw_parameters)?;
+        Ok(())
+    }
+
+    pub fn print(self: &Self, target: &mut glium::Frame, s: &str) -> Result<(), glium::DrawError> {
+        let mut x_offset = 0.0;
+
         for c in s.chars() {
             let n = self.char_to_num.get(&(c as i64));
+            let cd = self.fd.data.get(&(c as i64));
+
+
             if let (Some(n)) = n {
                 let ci = *n as usize * 4;
-                target.draw(self.vbo.slice(ci..(ci+4)).unwrap(), &NO_INDICES, &self.program, &uniforms, &draw_parameters)?;
+
+                self.print_single(target, ci, [x_offset, 0.0])?;
+                x_offset += cd.unwrap().x_advance as f32;
             }
         }
 
